10458af5018135549276cd39135dc4f2b7dc04b3
Pavan0518 <47776993+Pavan0518@users.noreply.github.com>
2025-07-15 11:14:56 +0530

    If condition.

---
+++ Conditional_Branching/If_Condition.py
@@ -0,0 +1,6 @@
+x = 6
+r = x % 2
+if r == 0:
+    print('Even number.')
+else:
+    print('Odd number.')
---
+++ Conditional_Branching/If_Condition2.py
@@ -0,0 +1,13 @@
+x = 6
+r = x % 2
+if r == 0:
+    print('Even number.')
+
+if r == 1:
+    print('Odd number.')
+
+if r == 2:
+    print('Even or odd.')
+
+if r == 3:
+    print('Odd or even.')
